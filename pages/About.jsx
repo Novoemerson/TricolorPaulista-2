783f903708178450c7c69ef9039e0cf29c4e2258
Novoemerson <novoemerson@gmail.com>
2025-05-22 00:52:29 -0300

    Create About.jsx

---
+++ pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>Sobre o Tricolor Paulista</h1>
+      <p>Portal não oficial dedicado à torcida do São Paulo FC.</p>
+    </div>
+  );
+}
