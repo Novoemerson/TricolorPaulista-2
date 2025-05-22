22da5ba40f0329c705ea04641316e8f4aeb6f944
Novoemerson <novoemerson@gmail.com>
2025-05-22 01:09:51 -0300

    Create Matches.jsx

---
+++ src/pages/Matches.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Matches() {
+  return (
+    <div>
+      <h1>Jogos do Tricolor Paulista</h1>
+      <p>Agenda dos próximos jogos e resultados recentes.</p>
+    </div>
+  );
+}
